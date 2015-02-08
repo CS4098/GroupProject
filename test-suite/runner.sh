fc304541abcb8fbf079cd651f180daa96c556120
soniloi <niloinso@tcd.ie>
2015-02-08 18:13:43 +0000

    Test-runner set up for logging and checking for necessary files

--- test-suite/runner.sh
+++ test-suite/runner.sh
@@ -22,11 +22,42 @@ if ! [[ -d $suite ]]; then
 	exit 1
 fi
 
+# Set up logging
+ts=(`date "+%F-%H-%M-%S"`)
+logfile="$ts.log"
+touch $logfile
+
+# Set up counts
+count_total=0
+count_failed=0
+
+# Process input tests
 for dir in $2/*/
 do
-	echo $dir
-	for file in $dir/*
+	echo "[--- $dir ---]" >> $logfile
+	for filepath in $dir/*.pml
 	do
-		echo $file
+		pml_filename=(`basename $filepath`)
+		echo -e "\tTest found: $pml_filename" >> $logfile
+
+		basename="${pml_filename%.*}"
+
+		expected_filepath="$dir$basename.pml.expected"
+		if ! [[ -f  $expected_filepath ]]; then
+			echo -e "\tError: expected promela file not found, skipping." >> $logfile
+			continue
+		fi
+		expected_filename=(`basename $expected_filepath`)
+		echo -e "\tExpected promela file found: $expected_filename" >> $logfile
+		count_total=$((count_total+1))
+
+		echo >> $logfile
 	done
 done
+
+# Print summary to file and console
+count_succeeded=$(($count_total-$count_failed))
+summary="[=== SUMMARY ===]\n\tTOTAL: $count_total\n\tFAILED: $count_failed\n\tSUCCEEDED: $count_succeeded"
+echo -e $summary >> $logfile
+echo -e $summary
+echo -e "\nLog available at: $logfile"
