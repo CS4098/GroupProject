6bb376afcd38d0dccf81d2dfbccce535224d1ce0
Cathal Geoghegan <pinpinire@gmail.com>
2015-02-09 18:42:32 +0000

    Fix launch script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR1="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR2="$( cd ../pml-bnfc && pwd )"
-docker run -v "$DIR1:/opt/group-project" -v "$DIR2:/opt/pml-bnfc" -t -i cs4098/test1 /bin/bash
+docker run -v "$DIR1:/opt/group-project" -v "$DIR2:/opt/pml-bnfc" -t -i cs4098/groupproject /bin/bash
