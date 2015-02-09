2f18416edccfaefcf5ed07ba3b5ae1bde4ebbbc2
Cathal Geoghegan <pinpinire@gmail.com>
2015-02-09 18:37:57 +0000

    Update readme and launch script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -v "$DIR:/opt/group-project" -t -i cs4098/groupproject /bin/bash
+DIR1="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR2="$( cd ../pml-bnfc && pwd )"
+docker run -v "$DIR1:/opt/group-project" -v "$DIR2:/opt/pml-bnfc" -t -i cs4098/test1 /bin/bash
