fe4ffecc704f7d36e4afb360e82b5cf6dd457e94
soniloi <niloinso@tcd.ie>
2015-02-13 22:58:06 +0000

    XML Translator needed to be passed output filename

--- src/main/translator-xml/TranslateXML.py
+++ src/main/translator-xml/TranslateXML.py
@@ -4,14 +4,14 @@ import sys
 from xml.dom import minidom
 
 def main():
-	if len(sys.argv) < 2:
-		print "Usage: " + sys.argv[0] + " <filename.xml>"
+	if len(sys.argv) < 3:
+		print "Usage: " + sys.argv[0] + " <input-filename.xml> <output-filename.pml>"
 		sys.exit(1)
 
-	filename = sys.argv[1]
+	input_filename = sys.argv[1]
 
 	try:
-		pmltree = minidom.parse(filename)
+		pmltree = minidom.parse(input_filename)
 		print pmltree.toxml()
 	except IOError:
 		print "File IO error."
