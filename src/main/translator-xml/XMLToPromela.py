6d78e9569c151bc611f88ee613ef8576fa7d773a
Cathal Geoghegan <pinpinire@gmail.com>
2015-02-16 19:56:29 +0000

    Add argparse to program

--- src/main/translator-xml/XMLToPromela.py
+++ src/main/translator-xml/XMLToPromela.py
@@ -3,31 +3,47 @@
 import sys
 import XMLTranslator
 
+
 # Read in an XML file representing an AST and output the corresponding Promela file
-def translate_xml_file(input_filename, output_filename):
+def translate_xml_file(xml_file, promela_file):
 
-  # Handle input file
-  try:
-    xml_file = open(input_filename, 'r')
-    xml_string = xml_file.read()
-    xml_file.close()
+    # Handle input file
+    try:
+        xml_string = xml_file.read()
+        xml_file.close()
 
-  except IOError:
-    print "Error processing input file, exiting."
-    sys.exit(1)
+    except IOError:
+        print "Error processing input file, exiting."
+        sys.exit(1)
 
-  # Perform translation
-  translator = XMLTranslator.XMLTranslator()
-  translation = translator.translate_xml(xml_string)
+    # Perform translation
+    translator = XMLTranslator.XMLTranslator()
+    translation = translator.translate_xml(xml_string)
 
-  # Handle output file
-  try:
-    # Read XML file
-    promela_file = open(output_filename, 'w')
-    for line in translation:
-      promela_file.write(line + '\n')
-    promela_file.close()
+    # Handle output file
+    try:
+        # Read XML file
+        for line in translation:
+            promela_file.write(line + '\n')
+        promela_file.close()
 
-  except IOError:
-    print "Error processing output file, exiting."
-    sys.exit(1)
+    except IOError:
+        print "Error processing output file, exiting."
+        sys.exit(1)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="XML to promela code translator")
+    parser.add_argument('-x', '--xml', required=True, type=file, help="Input abstract syntax tree in XML format")
+    parser.add_argument('-p', '--pl', required=True, type=argparse.FileType('w'), help="Output promela file")
+
+    try:
+        args = parser.parse_args()
+        translate_xml_file(args.xml, args.pl)
+    except IOError, msg:
+        parser.error(str(msg))
+
+
+if __name__ == "__main__":
+    main()
