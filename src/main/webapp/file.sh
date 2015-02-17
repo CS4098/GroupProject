601cbcc812f4f0620a08858c4faf4afa9282ffac
Darragh <hickeyd1@tcd.ie>
2015-02-17 20:04:09 +0000

    cgi script now saves input file and is set up to call another program with the file as an input, it's not the most secure at the moment though

--- src/main/webapp/file.sh
+++ src/main/webapp/file.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo "test script for calling from python cgi\n"
+file=$1
+
+echo "this is the file.sh output, a test script for calling from python cgi\n"
+
+while read line
+do
+    echo "<p>$line"
+done < $file
