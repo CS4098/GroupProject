08625741c199e1ec32d0fc78f6c3e2bbcfa979a7
soniloi <niloinso@tcd.ie>
2015-02-22 14:26:04 +0000

    Factored common parts of test and verify scripts into check script

--- src/main/model-checker/model-verify.sh
+++ src/main/model-checker/model-verify.sh
@@ -1,63 +1,11 @@
 #!/bin/bash
 
-# Run Spin in test mode (single path of execution)
-
-usage="Usage: $0 <path-to-input-PML-file> <path-to-Spin-output-file>"
-promelatmp="modeltest_tmp.promela"
-spin="spin"
-
-# Determine path of PML-to-Promela script
-scriptdir=(`dirname ${BASH_SOURCE[0]}`)
-pmltopromela="$scriptdir/../translator-xml/PMLToPromela.sh"
-
-# Check number of parameters
-if [[ "$#" -ne 2 ]]; then
-	echo $usage
-	exit 1
-fi
-
-# Check parameters
-pmlfile=$1
-if ! [[ -f $pmlfile ]]; then
-	echo "$usage: given path to input PML file does not exist or is not a regular file."
-	exit 1
-fi
-
-outputfile=$2
-outputdir=(`dirname $outputfile`)
-if ! [[ -d $outputdir ]]; then
-	mkdir -p $outputdir
-fi
-
-# Run translator
-$pmltopromela $pmlfile $promelatmp
-
-if ! [[ -f $promelatmp ]]; then
-	echo "Error: no Promela file created, exiting."
-	exit 1
-fi
-
 # Run Spin in verification mode
-$spin -a $promelatmp
+# FIXME: extend params to allow selection of specific claims
 
-if ! [[ -f "pan.c" ]]; then
-	echo "Error: no pan.c file generated, exiting."
-	exit 1
-fi
+# Determine path of check script
+scriptdir=(`dirname ${BASH_SOURCE[0]}`)
+check="$scriptdir/model-check.sh"
 
-gcc -O2 -DSAFETY -o pan pan.c
-
-if ! [[ -x "pan" ]]; then
-	echo "Error: no pan executable generated, exiting."
-	exit 1
-fi
-
-./pan > $outputfile 2>&1
-
-# Clean temporaries
-rm $promelatmp pan.? pan
-
-# FIXME: maybe do something with the trail files later; for now, erase this artefact
-if [[ -f $promelatmp.trail ]]; then
-	rm $promelatmp.trail
-fi
+# Run check
+$check "$@" verify
