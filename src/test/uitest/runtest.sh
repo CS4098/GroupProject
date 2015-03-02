e2386a483d5bec4be4f275e069d0a356860405fb
Cathal Geoghegan <pinpinire@gmail.com>
2015-03-02 18:20:47 +0000

    Return fails in ui test

--- src/test/uitest/runtest.sh
+++ src/test/uitest/runtest.sh
@@ -1,7 +1,10 @@
-#pip install selenium
+#!/bin/sh
+count_failed=0
+
 java -jar ./src/test/uitest/selenium-server-standalone-2.43.1.jar &
 sleep 5
-python ./src/test/uitest/uitest.py
+fails=$(python ./src/test/uitest/uitest.py)
 kill $!
 sleep 1
-echo
+count_failed=$((count_failed+fails))
+exit $count_failed
